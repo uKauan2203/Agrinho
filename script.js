9e289170df387c968c5fc44ad9c0606c535e3082
uKauan2203 <164882067+uKauan2203@users.noreply.github.com>
2024-08-09 11:33:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Obter referência ao botão de voltar ao topo
 const backToTopButton = document.getElementById('backToTop');
 
-// Mostrar o botão quando o usuário rolar para baixo
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) { // Ajuste o valor conforme necessário
+    if (window.scrollY > 300) { 
         backToTopButton.style.display = 'block';
     } else {
         backToTopButton.style.display = 'none';
     }
 });
 
-// Adicionar evento de clique para rolar para o topo
 backToTopButton.addEventListener('click', function() {
     window.scrollTo({
         top: 0,
